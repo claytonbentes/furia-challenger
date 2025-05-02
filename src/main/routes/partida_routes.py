8f864f4d9eb3b89b922a9355c28b86dd88d3cac2
Clayton Bentes <claytonbentes@live.com>
2025-05-02 19:15:58 -0300

    feat: implementando rotas

---
+++ src/main/routes/partida_routes.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.data.partida_handler import PartidaHandler
+from src.errors.error_handler import handle_error
+
+player_route_bp = Blueprint("partida_route", __name__)
+
+@partida_route_bp.route("/partidas", methods=["POST"])
+def create_partida():
+    try:
+        http_request = HttpRequest(body=request.json)
+        partida_handler = PartidaHandler()
+
+        http_response = partida_handler.register(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
+
+@partida_route_bp.route("/partidas/<partida_id>", methods=["GET"])
+def get_partida(partida_id):
+    try:
+
+        partida_handler = PartidaHandler()
+        http_request = HttpRequest(param={"partida_id": partida_id})
+        http_response = partida_id.find_by_id(http_request)
+
+        return jsonify(http_response.body), http_response.status_code
+
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
+
+@player_route_bp.route("/partidas", methods=["GET"])
+def get_partidas():
+    try:
+        partida_handler = PartidaHandler()
+        http_request = HttpRequest()
+        http_response = partida_handler.get_all_partidas(http_request)
+
+        return jsonify(http_response.body), http_response.status_code
+
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
---
+++ src/main/server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+from src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
+
+app = Flask(__name__)
+CORS(app)
+
+from src.main.routes.player_routes import player_route_bp
+
+app.register_blueprint(player_route_bp)
